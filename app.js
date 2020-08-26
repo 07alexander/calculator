69d836d2b227fcb1e2747a76f25e3c99a943bf8a
07alexander <07alexander@gmail.com>
2020-08-26 13:56:36 -0700

    fixed zero error

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-function add(x, y){
-    return x + y;
-}
+let total=0;
 
-console.log(add(4,5))
+function add(x, y){
+    return Math.round(x + y);
+}
 
 function subtract(x, y){
     return x - y;
 }
 
-console.log(subtract(4, 5))
-
 function multiply(x,y){
     return x * y;
 }
 
-console.log(multiply(4, 5))
 
 function divide(x, y){
     return x / y;
 }
 
-console.log(divide(4, 5))
 
 function operate(operator, x, y){
     let answer;
@@ -38,21 +34,91 @@ function operate(operator, x, y){
         case "/":
             answer = divide(x, y);
     }
-    console.log(x + operator + y + "=" + answer)
 }
 
-operate("/",5,6);
-
-document.getElementById("keys").addEventListener("click",function(){
-    let value = keys(document.getElementById)
-})
-
-/*const numbers = document.querySelectorAll('.number')
-numbers.forEach(function(currentNumber){
-    currentNumber.addEventListener('click', console.log(currentNumber.value));
-})
-
-function getValue(){
-
-}
+document.getElementById("+").addEventListener("click", function(){
+    curr=document.getElementById("screen").value;
+    total = add(total, curr);
+    document.getElementById("screen").value = total;
+});
+document.getElementById("-").addEventListener("click", function(){
+    document.getElementById("screen").value += "-";
+});
+document.getElementById("*").addEventListener("click", function(){
+    document.getElementById("screen").value += "*";
+});
+document.getElementById("/").addEventListener("click", function(){
+    document.getElementById("screen").value += "/";
+});
+document.getElementById("9").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 9
+    }else
+    document.getElementById("screen").value += 9;
+});
+document.getElementById("8").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 8
+    }else
+    document.getElementById("screen").value += 8;
+});
+document.getElementById("7").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 7
+    }else
+    document.getElementById("screen").value += 7;
+});
+document.getElementById("6").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 6
+    }else
+    document.getElementById("screen").value += 6;
+});
+document.getElementById("5").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 5
+    }else
+    document.getElementById("screen").value += 5;
+});
+document.getElementById("4").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 4
+    }else
+    document.getElementById("screen").value += 4;
+});
+document.getElementById("3").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 3
+    }else
+    document.getElementById("screen").value += 3;
+});
+document.getElementById("2").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 2
+    }else
+    document.getElementById("screen").value += 2;
+});
+document.getElementById("1").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 1
+    }else
+    document.getElementById("screen").value += 1;
+});
+document.getElementById("0").addEventListener("click", function(){
+    if(document.getElementById("screen").value == 0){
+        document.getElementById("screen").value = 0
+    }else
+    document.getElementById("screen").value += 0;
+});
+/*
+document.getElementById(".").addEventListener("click", function(){
+    document.getElementById("screen").value += ".";
+});
 */
+document.getElementById("all-clear").addEventListener("click", function(){
+    document.getElementById("screen").value = '0';
+    console.log(document.getElementById("screen").value)
+});
+document.getElementById("=").addEventListener("click", function(){
+    console.log(document.getElementById("screen").value)
+});
