5553f002a764fc02058b5b9d23a9f2ce4782e321
07alexander <07alexander@gmail.com>
2022-04-20 13:48:38 -0700

    added functions for add,subtract,multiply,divide

---
+++ app.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    var answer = a + b;
+    return answer;
+}
+function subtract(a, b){
+    var answer = a - b;
+    return answer;
+}
+function multiply(a, b){
+    var answer = a * b;
+    return answer;
+}
+function divide(a, b){
+    var answer = a / b;
+    return answer;
+}
+
+console.log(add(12,2));
+console.log(subtract(12,2));
+console.log(multiply(12,2));
+console.log(divide(12,2));
